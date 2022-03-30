c1dfcb784290e8b39c8c835208b8f86e44b060f1
chiaraamerio <100305899+chiaraamerio@users.noreply.github.com>
2022-03-30 16:25:24 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,18 @@
 using namespace std;
 
 int main() {
-   return 0;
+   int n,a=0,b=1,c;
+   cout<<"inserire un valore intero n"<<endl;
+   cin>>n;
+   int k=0;
+   if(n>=2){
+   while (k<n) {
+      c=a+b;
+      cout<<c<<endl;
+     b=a;
+     a=c;
+   }}else{
+      cout<<"errore"<<endl;
+   }
+return 0;
 }
